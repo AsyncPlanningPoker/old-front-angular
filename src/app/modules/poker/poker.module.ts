0dd3b03cbc54589ecda46d7f2d59371e8affe175
EdsonMartinelli <martinelli1997@gmail.com>
2021-12-06 17:59:02 -0300

    Move Story to Game

--- src/app/modules/poker/poker.module.ts
+++ src/app/modules/poker/poker.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from "@angular/core"
 import { CommonModule } from "@angular/common"
-
 import { PokerRoutingModule } from "./poker-routing.module"
 import { PokerCreateComponent } from "./components/poker-create/poker-create.component"
 import { PokerTableComponent } from "./components/poker-table/poker-table.component"
 import { SharedModule } from "src/app/shared/shared.module"
-import { StoryModule } from "../story/story.module"
 import { PokerItemComponent } from "./components/poker-item/poker-item.component"
 import { ConfirmDeleteComponent } from "./components/confirm-delete/confirm-delete.component"
 import { ConfirmUpdateComponent } from "./components/confirm-update/confirm-update.component"
-import { UserFromPokerModule } from "../user-from-poker/user-from-poker.module"
 
 @NgModule({
 	declarations: [
@@ -23,8 +20,6 @@ import { UserFromPokerModule } from "../user-from-poker/user-from-poker.module"
 		CommonModule,
 		SharedModule,
 		PokerRoutingModule,
-		StoryModule,
-		UserFromPokerModule
 	],
 	exports: [PokerCreateComponent, PokerTableComponent]
 })
